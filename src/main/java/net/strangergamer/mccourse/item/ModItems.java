824438f564d6b8767b320bfe2618876495777dd6
StrangerGamer-A <mazaroespy@gmail.com>
2024-10-02 09:47:12 -0700

    Added A Custom Advanced Item

--- src/main/java/net/strangergamer/mccourse/item/ModItems.java
+++ src/main/java/net/strangergamer/mccourse/item/ModItems.java
@@ -5,12 +5,16 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.strangergamer.mccourse.MCCourseMod;
+import net.strangergamer.mccourse.item.custom.ChainsawItem;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MCCourseMod.MOD_ID);
 
     public static final DeferredItem<Item> BLACK_OPAL = ITEMS.registerSimpleItem("black_opal");
     public static final DeferredItem<Item> RAW_BLACK_OPAL = ITEMS.registerSimpleItem("raw_black_opal");
+
+    public static final DeferredItem<Item> CHAINSAW =
+            ITEMS.registerItem("chainsaw", ChainsawItem::new, new Item.Properties().durability(32));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
